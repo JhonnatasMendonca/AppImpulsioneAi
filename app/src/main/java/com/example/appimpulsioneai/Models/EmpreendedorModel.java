adf82cc8b2e75e0c9d59067a5491179d9970a52f
JhonnatasMendonca <jhonnatas.m2000@gmail.com>
2024-06-07 09:39:13 -0300

    Edit feat: resolvendo bugs.

--- app/src/main/java/com/example/appimpulsioneai/Models/EmpreendedorModel.java
+++ app/src/main/java/com/example/appimpulsioneai/Models/EmpreendedorModel.java
@@ -1,25 +1,27 @@
 package com.example.appimpulsioneai.Models;
 
+
 import com.google.gson.annotations.SerializedName;
 
+
 public class EmpreendedorModel {
-
-    
-
-    // Getters and setters
     @SerializedName("idEmpreededor")
     private String id;
+
 
     @SerializedName("biografia")
     private String biografia;
 
+
     @SerializedName("numeroFavoritos")
     private int numeroFavoritos;
+
 
     @SerializedName("telefone")
     private String telefone;
     @SerializedName("site")
     private String site;
+
 
     @SerializedName("nomeCompleto")
     private String nomeCompleto;
@@ -50,180 +52,186 @@ public class EmpreendedorModel {
     @SerializedName("numeroVisitas")
     private int numeroVisitas;
 
+
     public String getId() {
         return id;
     }
+
 
     public String getBiografia() {
         return biografia;
     }
 
+
     public void setBiografia(String biografia) {
         this.biografia = biografia;
     }
+
 
     public int getNumeroFavoritos() {
         return numeroFavoritos;
     }
 
+
     public void setNumeroFavoritos(int numeroFavoritos) {
         this.numeroFavoritos = numeroFavoritos;
     }
+
 
     public String getTelefone() {
         return telefone;
     }
 
+
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
+
 
     public String getSite() {
         return site;
     }
 
+
     public void setSite(String site) {
         this.site = site;
     }
+
 
     public String getNomeCompleto() {
         return nomeCompleto;
     }
 
+
     public void setNomeCompleto(String nomeCompleto) {
         this.nomeCompleto = nomeCompleto;
     }
+
 
     public String getCpf() {
         return cpf;
     }
 
+
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
+
 
     public String getMei() {
         return mei;
     }
 
+
     public void setMei(String mei) {
         this.mei = mei;
     }
+
 
     public boolean isVerificado() {
         return verificado;
     }
 
+
     public void setVerificado(boolean verificado) {
         this.verificado = verificado;
     }
+
 
     public String getSenha() {
         return senha;
     }
 
+
     public void setSenha(String senha) {
         this.senha = senha;
     }
+
 
     public String getNomeEmpreendimento() {
         return nomeEmpreendimento;
     }
 
+
     public void setNomeEmpreendimento(String nomeEmpreendimento) {
         this.nomeEmpreendimento = nomeEmpreendimento;
     }
 
-    public Nicho getNicho() {
-        return nicho;
-    }
 
-    public void setNicho(Nicho nicho) {
-        this.nicho = nicho;
-    }
-
-    public static class Nicho {
-        @SerializedName("nicho")
-        private String nicho;
-
-        @SerializedName("id")
-        private int id;
-
-        // Getters and setters
-
-        
-
-        public void setNicho(String nicho) {
-            this.nicho = nicho;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-        
     public String getEmail() {
         return email;
     }
+
 
     public void setEmail(String email) {
         this.email = email;
     }
 
+
     public String getPlanoAssinatura() {
         return planoAssinatura;
     }
+
 
     public void setPlanoAssinatura(String planoAssinatura) {
         this.planoAssinatura = planoAssinatura;
     }
 
+
     public String getFacebook() {
         return facebook;
     }
+
 
     public void setFacebook(String facebook) {
         this.facebook = facebook;
     }
 
+
     public String getInstagram() {
         return instagram;
     }
+
 
     public void setInstagram(String instagram) {
         this.instagram = instagram;
     }
 
+
     public String getNomeExibicao() {
         return nomeExibicao;
     }
+
 
     public void setNomeExibicao(String nomeExibicao) {
         this.nomeExibicao = nomeExibicao;
     }
 
+
     public NichoModel getNicho() {
         return nicho;
     }
+
 
     public void setNicho(NichoModel nicho) {
         this.nicho = nicho;
     }
 
+
     public String getModalidade() {
         return modalidade;
     }
+
 
     public void setModalidade(String modalidade) {
         this.modalidade = modalidade;
     }
 
+
     public int getNumeroVisitas() {
         return numeroVisitas;
     }
+
 
     public void setNumeroVisitas(int numeroVisitas) {
         this.numeroVisitas = numeroVisitas;
--- app/src/main/java/com/example/appimpulsioneai/Models/NichoModel.java
+++ app/src/main/java/com/example/appimpulsioneai/Models/NichoModel.java
@@ -1,24 +1,29 @@
 package com.example.appimpulsioneai.Models;
 
+
 import com.google.gson.annotations.SerializedName;
+
 
 public class NichoModel {
     @SerializedName("idNicho")
     private int id;
 
+
     @SerializedName("nicho")
     private String nome;
+
 
     public int getId() {
         return id;
     }
 
+
     public String getNome() {
         return nome;
     }
+
 
     public void setNome(String nome) {
         this.nome = nome;
     }
 }
-
